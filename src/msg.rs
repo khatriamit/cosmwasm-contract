b686f9baa22f78de6e2cd10ccde174ff2b4ed0b1
khatriamit <khatriamit054@gmail.com>
2023-04-30 17:07:17 +0545

    messgae for query response:

---
+++ src/msg.rs
@@ -0,0 +1,33 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+// msg is the way to communicate with contract using frontend or cli
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub struct InstantiateMsg {
+    pub admin_address: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecuteMsg {
+    CreatePoll { question: String },
+    Vote { question: String, choice: String },
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    CustomMsg { val: String },
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub struct CustomResponse {
+    val: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum MigrateMsg {}
